d43773804809f69c06c3935fb3f58f5347e7dba1
vaibhav kumar <vaibhavk2474@gmail.com>
2026-06-07 19:50:32 +0530

    updated resume

--- src/components/UI/ResumeDownloadLink/index.tsx
+++ src/components/UI/ResumeDownloadLink/index.tsx
@@ -5,16 +5,11 @@ import styles from "./ResumeDownloadLinkBtn.module.css";
 import DownloadIcon from "@/icons/DownloadIcon";
 
 function ResumeDownloadLinkBtn() {
-  return (
-    <Link
-      className={styles.download}
-      href="/pdfs/Vaibhav-kumar-resume.pdf"
-      target="_blank"
-      download
-    >
-      Resume <DownloadIcon />
-    </Link>
-  );
+	return (
+		<Link className={styles.download} href="/pdfs/VAIBHAV-KUMAR-FlowCV-Resume-2025.pdf" target="_blank" download>
+			Resume <DownloadIcon />
+		</Link>
+	);
 }
 
 export default ResumeDownloadLinkBtn;
