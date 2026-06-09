87648632ee09427abff637d1a0dc5c7279e7d43a
vaibhav kumar <vaibhavk2474@gmail.com>
2026-06-09 12:26:46 +0530

    fix label

--- src/components/UI/ResumeDownloadLink/index.tsx
+++ src/components/UI/ResumeDownloadLink/index.tsx
@@ -7,7 +7,7 @@ import styles from "./ResumeDownloadLinkBtn.module.css";
 function ResumeDownloadLinkBtn() {
 	return (
 		<Link title="Download Resume" className={styles.download} href="/pdfs/VAIBHAV_KUMAR_FlowCV_Resume_2026-06-09.pdf" target="_blank">
-			Resume download
+			Resume
 		</Link>
 	);
 }
