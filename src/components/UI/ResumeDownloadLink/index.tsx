0a2f93c3b82693f06a36fc157d32bcce1f84cc67
vaibhav kumar <vaibhavk2474@gmail.com>
2026-08-18 22:49:40 +0530

    fix link issue

--- src/components/UI/ResumeDownloadLink/index.tsx
+++ src/components/UI/ResumeDownloadLink/index.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 import styles from "./ResumeDownloadLinkBtn.module.css";
 
-function ResumeDownloadLinkBtn({ title, link, target }: { title?: string; link: string; target?: string }) {
+function ResumeDownloadLinkBtn({ title, link, target }: { title?: string; link?: string; target?: string }) {
 	return (
-		<Link title="Download Resume" className={styles.download} href={link} {...{ ...(target ? { target: target } : {}) }}>
+		<Link title="Download Resume" className={styles.download} href={link || ""} {...{ ...(target ? { target: target } : {}) }}>
 			{title || "Resume"}
 		</Link>
 	);
