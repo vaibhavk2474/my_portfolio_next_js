3ba9416b93ef73fed38e38edc74c8ccbfb1a648b
vaibhav kumar <vaibhavk2474@gmail.com>
2026-09-03 20:02:28 +0530

    fix issues

--- src/components/Layout/Navbar/DrawerMenu/index.tsx
+++ src/components/Layout/Navbar/DrawerMenu/index.tsx
@@ -19,107 +19,97 @@ import ThemeSwitch from "../ThemeSwitch";
 import ResumeDownloadLinkBtn from "@/components/UI/ResumeDownloadLink";
 
 const CustomDiv = styled("div")({
-  "@media (min-width:601px)": {
-    display: "none",
-  },
+	"@media (min-width:601px)": {
+		display: "none",
+	},
 });
 
 const MenuIconCustom = styled(MenuIcon)({
-  color: "rgb(var(--color-primary))",
-  width: "3rem",
-  height: "3rem",
+	color: "rgb(var(--color-primary))",
+	width: "3rem",
+	height: "3rem",
 });
 const CustomListItem = styled(ListItem)({
-  padding: "1rem 2rem",
+	padding: "1rem 2rem",
 
-  "& a": {
-    width: "100%",
-  },
+	"& a": {
+		width: "100%",
+	},
 
-  "&:hover": {
-    background: "#e3e3e3",
-  },
+	"&:hover": {
+		background: "#e3e3e3",
+	},
 });
 const CustomList = styled(List)({
-  marginTop: "2rem",
+	marginTop: "2rem",
 });
 
 const DownloadBtnCustom = styled(Box)({
-  margin: "1rem 0.5rem",
+	margin: "1rem 0.5rem",
 });
 
 type Anchor = "top" | "left" | "bottom" | "right";
 
 export default function DrawerMenu() {
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
+	const [state, setState] = React.useState({
+		top: false,
+		left: false,
+		bottom: false,
+		right: false,
+	});
 
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === "keydown" &&
-        ((event as React.KeyboardEvent).key === "Tab" ||
-          (event as React.KeyboardEvent).key === "Shift")
-      ) {
-        return;
-      }
+	const toggleDrawer = (anchor: Anchor, open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+		if (event.type === "keydown" && ((event as React.KeyboardEvent).key === "Tab" || (event as React.KeyboardEvent).key === "Shift")) {
+			return;
+		}
 
-      setState({ ...state, [anchor]: open });
-    };
+		setState({ ...state, [anchor]: open });
+	};
 
-  const list = (anchor: Anchor) => (
-    <Box
-      sx={{
-        width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
-        height: "100%",
+	const list = (anchor: Anchor) => (
+		<Box
+			sx={{
+				width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
+				height: "100%",
 
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <Box>
-        <CustomList>
-          {MENU_LINKS.map((cMenu, index) => (
-            <CustomListItem key={index} disablePadding>
-              <Link href={`${cMenu.link}`}>{cMenu.name}</Link>
-            </CustomListItem>
-          ))}
-        </CustomList>
-        <Divider />
-        <ThemeSwitch />
-      </Box>
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "space-between",
+			}}
+			role="presentation"
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		>
+			<Box>
+				<CustomList>
+					{MENU_LINKS.map((cMenu, index) => (
+						<CustomListItem key={index} disablePadding>
+							<Link href={`${cMenu.link}`}>{cMenu.name}</Link>
+						</CustomListItem>
+					))}
+				</CustomList>
+				<Divider />
+				<ThemeSwitch />
+			</Box>
 
-      <DownloadBtnCustom>
-        <ResumeDownloadLinkBtn />
-      </DownloadBtnCustom>
-    </Box>
-  );
+			<DownloadBtnCustom>
+				<ResumeDownloadLinkBtn title="Resume" link={"/pdfs/VAIBHAV_KUMAR_FlowCV_Resume_2026-07-09.pdf"} target="_blank" />
+			</DownloadBtnCustom>
+		</Box>
+	);
 
-  return (
-    <CustomDiv>
-      {(["right"] as const).map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>
-            <MenuIconCustom />
-          </Button>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
-    </CustomDiv>
-  );
+	return (
+		<CustomDiv>
+			{(["right"] as const).map((anchor) => (
+				<React.Fragment key={anchor}>
+					<Button onClick={toggleDrawer(anchor, true)}>
+						<MenuIconCustom />
+					</Button>
+					<Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+						{list(anchor)}
+					</Drawer>
+				</React.Fragment>
+			))}
+		</CustomDiv>
+	);
 }
